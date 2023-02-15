80f3a6283aa1f77ea8d28392d87baed4adadeace
Mrigakshi Pandey <mrigakshipandey@gmail.com>
2023-02-15 22:21:21 +0530

    corrects back end url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverUrl: "http://ec2-15-207-115-77.ap-south-1.compute.amazonaws.com/api"
+  serverUrl: "http://ec2-15-207-115-77.ap-south-1.compute.amazonaws.com/api/"
 };
